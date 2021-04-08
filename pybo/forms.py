b74a42f66918c6d75c57ce2f4f130b0e1b32938e
Dobuzi <kimjw7981@gmail.com>
2021-04-08 15:51:52 +0900

    add signup page

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, PasswordField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
 
 class QuestionForm(FlaskForm):
     subject = StringField('Title', validators=[DataRequired('제목은 필수 입력 항목 입니다.')])
@@ -8,3 +9,9 @@ class QuestionForm(FlaskForm):
 
 class AnswerForm(FlaskForm):
     content = TextAreaField('Content', validators=[DataRequired('내용은 필수 입력 항목입니다.')])
+
+class UserCreateForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=3, max=25)])
+    password1 = PasswordField('Password', validators=[DataRequired(), EqualTo('password2', '비밀번호가 일치하지 않습니다.')])
+    password2 = PasswordField('Check Password', validators=[DataRequired()])
+    email = EmailField('Email', validators=[DataRequired(), Email()])
