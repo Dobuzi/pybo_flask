7e9d9db528cab14c38a26e79f5560afd42f6094f
Dobuzi <kimjw7981@gmail.com>
2021-04-12 14:12:39 +0900

    edit config 2

--- pybo.sh
+++ pybo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export FLASK_APP=pybo
 export FLASK_ENV=development
-export APP_CONFIG_FILE=/Users/dobuzi/Workspace/webApp/flask/pybo/config/development.py
+export APP_CONFIG_FILE=/Users/dobuzi/Workspace/webApp/flask/config/development.py
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 flask run
