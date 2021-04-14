34d84a43891093ebad0ebf08f69650d4d4c29535
Dobuzi <kimjw7981@gmail.com>
2021-04-14 22:13:47 +0900

    log on pybo

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, current_app
 from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
+    current_app.logger.info("print on info level")
     return redirect(url_for('question._list'))
